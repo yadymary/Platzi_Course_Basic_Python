f658a68ed3ef5e5780c81a07fcbe7a0047b5a64b
YadyMary <yadymarym@gmail.com>
2021-11-13 14:32:57 -0500

    Programa para validar la mayoría de edad

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+edad = int(input('Escribe tu edad: '))
+
+if edad > 18:
+    print('Eres mayor de edad')
+elif edad == 18:
+    print('Justo tienes la mayoría de edad exacta')
+else:
+    print('Eres menor de edad')
